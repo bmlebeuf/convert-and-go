a9e209c4b448fc5b6cc42030e9459aca2e6028d3
allisalebeuf <lebeua@uw.edu>
2018-06-25 17:43:34 -0700

    adding currency selector component and data

---
+++ src/ConvertApp/DataCurrency.js
@@ -0,0 +1,43 @@
+const data = {
+    "currencies": [
+        {
+            "code": "USD",
+            "sellRate": 1,
+            "name": "United States Dollars",
+            "sign": "$"
+
+        },
+        {
+            "code": "AUD",
+            "sellRate": 1.34,
+            "name": "Australian Dollars",
+            "sign": "$"
+        },
+        {
+            "code": "CAD",
+            "sellRate": 1.32,
+            "name": "Canadian Dollars",
+            "sign": "$"
+        },
+        {
+            "code": "NZD",
+            "sellRate": 1.45,
+            "name": "New Zealand Dollars",
+            "sign": "$"
+        },
+        {
+            "code": "JPY",
+            "sellRate": 109.52,
+            "name": "Japanese Yen",
+            "sign": "¥"
+        },
+        {
+            "code": "GBP",
+            "sellRate": 0.75,
+            "name": "British Pounds Sterling",
+            "sign": "£"
+        }
+    ]
+}
+
+export default data;
